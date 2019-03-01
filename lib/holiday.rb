f38b5fbbb48fb5ab01bd6a6e6982cba03ba5afea
Natasha Chernis <natashachernis@gmail.com>
2019-03-01 03:02:29 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -74,7 +74,7 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   holiday_hash.map do |season, holidays|
     holidays.map {|holiday, supplies| holiday if supplies.include?("BBQ")}
-    
+
   end
 
 end
